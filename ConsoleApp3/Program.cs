ab90c7723f71a435c63e63af4e459061985d1eba
mPHO.mODISE <mPHO.mODISE@DDNADBM1FD22>
2025-11-27 14:25:01 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+{
+    class ConsoleApp
+    {
+        static void Main(string[] args) {
+
+            Console.Write("Hi, this is my first console program.\nI'm new here so please be kind, here is what I\'ve learnt so far: ");
+            Console.WriteLine("1. Data Types - There are 5 data types in c#.");
+
+            int age = 21;
+            double cash = 17.50;
+            char userName= '@';
+            string name= "Mpho";
+            bool gender = false;
+
+            Console.WriteLine("Yes, I did this all by myself...Here is the result.");
+            Console.WriteLine("My name is " + name + ",I am " + age + " years old.");
+            Console.WriteLine("Currently I have R " + cash + ".You are probably asking yourself how that is relevant...Well I just thought I could share.");
+            Console.WriteLine("Am i male ? " + gender + "\nThen what am I ?");
+            Console.WriteLine("Here is my username: " + userName + name);
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
